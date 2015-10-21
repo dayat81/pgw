e3502e368a708647c0fd0f16dc6baae6700e18a6
Hidayat Hidayat <uda.dayat@gmail.com>
2015-10-21 09:32:44 +0700

    Initial Commit

---
+++ pgw/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pgw
+//
+//  Created by hidayat on 10/21/15.
+//  Copyright © 2015 hidayat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
